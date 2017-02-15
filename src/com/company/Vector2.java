f06d2e40f5108431a58ed46fa61a7136c73fc92c
DawgoBell1 <dgarz0610@yahoo.com>
2017-02-14 22:26:23 -0600

    moved dx and dy . added magnitude to vector class.

--- src/com/company/Vector2.java
+++ src/com/company/Vector2.java
@@ -6,15 +6,33 @@ package com.company;
 public class Vector2 {
     public float x;
     public float y;
-    public Vector2(){
+    public float magnitude;
+
+    public Vector2() {
 
     }
-    public Vector2(Vector2 v){
-        this.x=v.x;
-        this.y=v.y;
+
+    public Vector2(Vector2 v) {
+        this.x = v.x;
+        this.y = v.y;
     }
-    public Vector2(float x,float y){
-        this.x=x;
-        this.y=y;
+
+    public Vector2(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Vector2(float x, float y, float magnitude) {
+        this.x = x;
+        this.y = y;
+        this.magnitude = magnitude;
+    }
+
+    public float dx(Vector2 a) {
+        return this.x - a.x;
+    }
+
+    public float dy(Vector2 a) {
+        return this.y - a.y;
     }
 }
