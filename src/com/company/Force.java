5f66c94b6720819dc29db2872a701fd754dba41b
DawgoBell1 <dgarz0610@yahoo.com>
2017-02-15 09:43:22 -0600

    made Force class with setters and getters

--- src/com/company/Force.java
+++ src/com/company/Force.java
@@ -3,13 +3,34 @@ package com.company;
 /**
  * Created by Dago on 2/15/2017.
  */
-public class Force extends Vector2{
+public class Force extends Vector2 {
+    public float magnitude;
+    public float angle;
 
-    public Force(){
+    public Force() {
 
     }
-    public Force(float magnitude){
-        this.magnitude=magnitude;
+
+    public Force(float magnitude) {
+        this.magnitude = magnitude;
     }
+
+    public float getMagnitude() {
+        return magnitude;
+
+    }
+
+    public float getAngle() {
+        return angle;
+    }
+
+    public void setMagnitude(float magnitude) {
+        this.magnitude = magnitude;
+    }
+
+    public void setAngle(float angle) {
+        this.angle = angle;
+    }
+
 
 }
