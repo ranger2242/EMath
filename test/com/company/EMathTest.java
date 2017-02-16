991317034be713f4c3c87283fb33ba955d6a0c7f
DawgoBell1 <dgarz0610@yahoo.com>
2017-02-16 01:26:23 -0600

    testing body

---
+++ test/com/company/EMathTest.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by Dago on 2/16/2017.
+ */
+class EMathTest {
+    @Test
+    void pathag() {
+        double d = EMath.pathag(0,0,0,50);
+        System.out.print(d);
+    }
+
+}
