b762b587233fe9dbe7b7fb5c8f769ea01b1e0327
Tyler Hurson <glassplotful@gmail.com>
2017-03-01 10:44:43 -0600

    Changed Constant to handle BigDecimals rather than doubles.

--- src/com/company/formula/expression/unary/Constant.java
+++ src/com/company/formula/expression/unary/Constant.java
@@ -1,16 +1,24 @@
 package com.company.formula.expression.unary;
 
+import java.math.BigDecimal;
+
 /**
  * Created by tyler on 2/28/17.
  */
 public class Constant implements UnaryExpression{
 
-    public double getNumber() {
+    public BigDecimal getConstant() {
 
         return number;
     }
 
-    private final double number;
+    public String getConstant(int sigFigs) {
+
+        // TODO
+        return "";
+    }
+
+    private final BigDecimal number;
 
     public Constant(int number) {
 
@@ -19,12 +27,22 @@ public class Constant implements UnaryExpression{
 
     public Constant(double number) {
 
+        this(new BigDecimal(number));
+    }
+
+    public Constant(String number) {
+
+        this(new BigDecimal(number));
+    }
+
+    public Constant(BigDecimal number) {
+
         this.number = number;
     }
 
     @Override
     public String toString() {
 
-        return "" + this.getNumber();
+        return "" + this.getConstant();
     }
 }
