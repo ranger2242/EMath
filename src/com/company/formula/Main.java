8f2f251d01baf10156019016374f39d607308e93
Tyler Hurson <glassplotful@gmail.com>
2017-03-01 09:51:15 -0600

    Added exponentation operator.

---
+++ src/com/company/formula/Main.java
@@ -0,0 +1,12 @@
+package com.company.formula;
+
+/**
+ * Created by tyler on 3/1/17.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("hi");
+    }
+}
